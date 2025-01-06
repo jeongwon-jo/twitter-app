b9eea137fe505d8c771a2b2c9ecc7caf6fd2536c
jeongwon-jo <jjeong1.jo@gmail.com>
2025-01-06 17:34:59 +0900

    [update] 회원가입 기능 추가
    [update] 회원가입 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
 import Router from 'components/Router';
 import { Layout } from 'components/Layout';
+import { getAuth, onAuthStateChanged } from "firebase/auth"
+import { app } from "firebaseApp"
+import { useState, useEffect } from 'react';
+import { ToastContainer } from 'react-toastify';
+
 
 function App() {
+	const auth = getAuth(app)
+	const [init, setInit] = useState<boolean>(false)
+  // currentUser이 있으면 isAuthenticated : true
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+		!!auth.currentUser
+	);
+
+	useEffect(() => {
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				setIsAuthenticated(true)
+			} else {
+				setIsAuthenticated(false);
+			}
+
+			setInit(true);
+		})
+	}, [auth])
+
   return (
-    <Layout>
-      <Router />
-    </Layout>
-  );
+		<Layout>
+			<ToastContainer />
+			{init ? <Router isAuthenticated={isAuthenticated} /> : "loading..."}
+		</Layout>
+	);
 }
 
 export default App;
--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -11,21 +11,33 @@ import LoginPage from "pages/users/login";
 import SignupPage from "pages/users/signup";
 import { Navigate, Route, Routes } from "react-router-dom";
 
-export default function Router() {
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/posts" element={<PostListPage />} />
-      <Route path="/posts/:id" element={<PostDetailPage />} />
-      <Route path="/posts/new" element={<PostNewPage />} />
-      <Route path="/posts/edit/:id" element={<PostEditPage />} />
-      <Route path="/profile" element={<ProfilePage />} />
-      <Route path="/profile/edit" element={<ProfileEditPage />} />
-      <Route path="/notification" element={<NotificationPage />} />
-      <Route path="/search" element={<SearchPage />} />
-      <Route path="/users/login" element={<LoginPage />} />
-      <Route path="/users/signup" element={<SignupPage />} />
-      <Route path="*" element={<Navigate replace to="/" />} />
-    </Routes>
-  )
+interface RouterProps {
+  isAuthenticated: boolean;
+}
+
+export default function Router({ isAuthenticated }: RouterProps) {
+	return (
+		<Routes>
+			{isAuthenticated ? (
+				<>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/posts" element={<PostListPage />} />
+					<Route path="/posts/:id" element={<PostDetailPage />} />
+					<Route path="/posts/new" element={<PostNewPage />} />
+					<Route path="/posts/edit/:id" element={<PostEditPage />} />
+					<Route path="/profile" element={<ProfilePage />} />
+					<Route path="/profile/edit" element={<ProfileEditPage />} />
+					<Route path="/notification" element={<NotificationPage />} />
+					<Route path="/search" element={<SearchPage />} />
+					<Route path="*" element={<Navigate replace to="/" />} />
+				</>
+			) : (
+				<>
+					<Route path="/users/login" element={<LoginPage />} />
+					<Route path="/users/signup" element={<SignupPage />} />
+					<Route path="*" element={<Navigate replace to="/users/login" />} />
+				</>
+			)}
+		</Routes>
+	);
 }
